c5888ae1656b8d486a1b8d92fdc8060b8b4d62f1
Mikalia Shchurko <maxe1.first@gmail.com>
2022-12-20 17:38:13 +0600

    Initial commit

---
+++ wikimedia-reader/src/main/java/by/mikalai/wikimedia/wikimediareader/WikimediaReaderApplication.java
@@ -0,0 +1,13 @@
+package by.mikalai.wikimedia.wikimediareader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaReaderApplication.class, args);
+	}
+
+}
---
+++ wikimedia-reader/src/test/java/by/mikalai/wikimedia/wikimediareader/WikimediaReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mikalai.wikimedia.wikimediareader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
